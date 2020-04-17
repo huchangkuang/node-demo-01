0c53fba2cce7f7d3c4dce94b003c50da4efc6eda
huchangkuang <603021137@qq.com>
2020-04-17 15:46:30 +0800

    v--2

--- server.js
+++ server.js
@@ -24,12 +24,22 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
+        response.write(`<!DOCTYPE html>
+        <html lang="zh-CN">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>server</title>
+        </head>
+        <body>
+            <h1>hello world</h1>
+        </body>
+        </html>`)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`body{color:red;}`)
         response.end()
     } else {
         response.statusCode = 404
